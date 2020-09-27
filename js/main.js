6b309d35b17187aa55c223a71f9ef892e91516ab
AlexM <a.morozov.work@mail.ru>
2020-09-27 12:03:53 +0300

    Complete all tasks

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
 const SIMILAR_AD_MAX_COUNT = 8;
+const PIN_OFFSET_X = 25;
+const PIN_OFFSET_Y = 70;
+// const MIN_PIN_POS_Y = 130; // temporarily not used
+// const MAX_PIN_POS_Y = 630; // temporarily not used
+const PINS_POS_INTERVAL_X = 220;
+const PINS_POS_INTERVAL_Y = 100;
 const APARTMENT_TYPES = [`palace`, `flat`, `house`, `bungalow`];
 const MIN_ROOMS_COUNT = 1;
 const MAX_ROOMS_COUNT = 3;
@@ -22,12 +28,43 @@ const PHOTOS_SRC = [
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
 ];
 
+const body = document.querySelector(`body`);
+const offset = parseInt((getComputedStyle(body, true).marginLeft), 10);
+const map = document.querySelector(`.map`);
+const mainPin = document.querySelector(`.map__pin--main`);
+const mapPinsList = document.querySelector(`.map__pins`);
+const mapPinTemplate = document.querySelector(`#pin`)
+  .content.querySelector(`.map__pin`);
+
 const getRandomCount = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
 
 const getRandomElement = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const getCoordinates = function (element) {
+  let box = element.getBoundingClientRect();
+
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset - offset
+  };
+};
+
+const getPinLocationX = function () {
+  let minPosX = getCoordinates(mainPin).left - PINS_POS_INTERVAL_X + PIN_OFFSET_X;
+  let maxPosX = getCoordinates(mainPin).left + PINS_POS_INTERVAL_X + PIN_OFFSET_X;
+
+  return getRandomCount(minPosX, maxPosX);
+};
+
+const getPinLocationY = function () {
+  let minPosY = getCoordinates(mainPin).top - PINS_POS_INTERVAL_Y - PIN_OFFSET_Y;
+  let maxPosY = getCoordinates(mainPin).top + PINS_POS_INTERVAL_Y - PIN_OFFSET_Y;
+
+  return getRandomCount(minPosY, maxPosY);
 };
 
 const getSimilarPins = function (count) {
@@ -39,20 +76,20 @@ const getSimilarPins = function (count) {
         avatar: `img/avatars/user0${i + 1}.png`
       },
       offer: {
-        title: ``,
-        address: `{{location.x}}, {{location.y}}`, // Replace with function
-        price: `{{price}}`, // Replace with function
+        title: `title`,
+        address: `${getPinLocationX()}, ${getPinLocationY()}`,
+        price: `{{price}}`, // Temporary value
         type: `${getRandomElement(APARTMENT_TYPES)}`,
-        rooms: `${getRandomCount(MIN_ROOMS_COUNT, MAX_ROOMS_COUNT)}`, // Must be the same with guests
-        guests: `${getRandomCount(MIN_GUESTS_COUNT, MAX_GUESTS_COUNT)}`,
-        checkin: `${getRandomElement(CHECKIN_TIMES)}`, // Must be the same with chackout
-        checkout: `${getRandomElement(CHECKOUT_TIMES)}`,
-        features: `${getRandomElement(FEATURES)}`, // need a shuffle function
+        rooms: `${getRandomCount(MIN_ROOMS_COUNT, MAX_ROOMS_COUNT)}`, // Temporary value
+        guests: `${getRandomCount(MIN_GUESTS_COUNT, MAX_GUESTS_COUNT)}`, // Temporary value
+        checkin: `${getRandomElement(CHECKIN_TIMES)}`, // Temporary value
+        checkout: `${getRandomElement(CHECKOUT_TIMES)}`, // Temporary value
+        features: `${getRandomElement(FEATURES)}`, // Need a shuffle function
         description: ``,
         photos: `${PHOTOS_SRC}`,
         location: {
-          x: ``, // Replace with function
-          y: `` // Replace with function
+          x: `${getPinLocationX()}`,
+          y: `${getPinLocationY()}`
         }
       }
     });
@@ -61,6 +98,23 @@ const getSimilarPins = function (count) {
   return similarPins;
 };
 
-const pins = getSimilarPins(SIMILAR_AD_MAX_COUNT);
+const createPins = function (pin) {
+  const mapPin = mapPinTemplate.cloneNode(true);
 
-console.log(pins);
+  mapPin.style = `left: ${pin.offer.location.x}px; top: ${pin.offer.location.y}px;`;
+  mapPin.querySelector(`img`).src = pin.author.avatar;
+  mapPin.querySelector(`img`).alt = pin.offer.title;
+
+  return mapPin;
+};
+
+const pins = getSimilarPins(SIMILAR_AD_MAX_COUNT);
+const fragment = document.createDocumentFragment();
+
+for (let i = 0; i < pins.length; i++) {
+  fragment.appendChild(createPins(pins[i]));
+}
+
+mapPinsList.appendChild(fragment);
+
+map.classList.remove(`map--faded`);
