107d3a5cde80ba283cec11422379ceb6f203c96f
AMorozov-web <AMorozov-web@users.noreply.github.com>
2020-10-23 15:19:04 +0300

    Create util and data modules

--- js/main.js
+++ js/main.js
@@ -1,28 +1,8 @@
 'use strict';
 
-const OFFERS_COUNT = 8;
 const MAIN_PIN_OFFSET_X = 65;
 const MAIN_PIN_OFFSET_Y = 65;
 const MAIN_PIN_ACTIVE_OFFSET_Y = 84;
-const PIN_OFFSET_X = 50;
-const PIN_OFFSET_Y = 70;
-const PINS_POS_INTERVAL_X = 220;
-const PINS_POS_INTERVAL_Y = 100;
-const MIN_ROOMS_COUNT = 1;
-const MAX_ROOMS_COUNT = 3;
-const MIN_GUESTS_COUNT = 1;
-const MAX_GUESTS_COUNT = 3;
-const APARTMENT_TYPES = [
-  `palace`,
-  `flat`,
-  `house`,
-  `bungalow`
-];
-const TIMES = [
-  `12:00`,
-  `13:00`,
-  `14:00`
-];
 const ROOMS_DECLENSION = [
   `комната`,
   `комнаты`,
@@ -33,42 +13,11 @@ const GUESTS_DECLENSION = [
   `гостей`,
   `гостей`
 ];
-const FEATURES = [
-  `wifi`,
-  `dishwasher`,
-  `parking`,
-  `washer`,
-  `elevator`,
-  `conditioner`
-];
-const PHOTOS_SRC = [
-  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
-  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
-  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
-];
 const typesMap = {
   'palace': `Дворец`,
   'flat': `Квартира`,
   'house': `Дом`,
   'bungalow': `Бунгало`
-};
-const titlesMap = {
-  'Дворец': [
-    `Огромный прекрасный дворец`,
-    `Маленький ужасный дворец`
-  ],
-  'Квартира': [
-    `Большая уютная квартира`,
-    `Маленькая неуютная квартира`
-  ],
-  'Дом': [
-    `Красивый гостевой домик`,
-    `Некрасивый негостеприимный домик`
-  ],
-  'Бунгало': [
-    `Уютное бунгало далеко от моря`,
-    `Неуютное бунгало по колено в воде`
-  ]
 };
 
 const map = document.querySelector(`.map`);
@@ -83,125 +32,7 @@ const mapPinsContainer = document.querySelector(`.map__pins`);
 const mapPinTemplate = document.querySelector(`#pin`)
   .content.querySelector(`.map__pin`);
 const popupCardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
-const pinsPosLimits = {
-  x: {
-    min: mainPinLeft - PINS_POS_INTERVAL_X - PIN_OFFSET_X / 2,
-    max: mainPinLeft + PINS_POS_INTERVAL_X - PIN_OFFSET_X / 2
-  },
-  y: {
-    min: mainPinTop - PINS_POS_INTERVAL_Y - PIN_OFFSET_Y,
-    max: mainPinTop + PINS_POS_INTERVAL_Y - PIN_OFFSET_Y
-  }
-};
 let isPageActive = false;
-
-const toggleFormElements = (parentElem, state = false) => {
-  for (let elem of parentElem.children) {
-    elem.disabled = state;
-  }
-};
-
-const shuffleArray = (arr) => {
-  const newArr = arr.slice();
-
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
-  }
-
-  return newArr;
-};
-
-const getRandomPrice = () => {
-  return Math.floor(Math.random() * 100) * 100;
-};
-
-const getRandomElement = (arr) => {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
-
-const getRandomInRange = (min, max) => {
-  return Math.floor(min + Math.random() * (max + 1 - min));
-};
-
-const getRandomArr = (arr) => {
-  const selectedElements = [];
-
-  if (arr.length !== 0) {
-    const randomElementsCount = getRandomInRange(1, arr.length);
-    const allElements = shuffleArray(arr);
-
-    for (let i = 0; i < randomElementsCount; i++) {
-      selectedElements.push(allElements[i]);
-    }
-  }
-
-  return selectedElements;
-};
-
-const declTextByNumber = (number, textWordsArr) => {
-  const a = Math.abs(number) % 100;
-  const b = number % 10;
-
-  if (a > 10 && a < 20) {
-    return textWordsArr[2];
-  }
-
-  if (b > 1 && b < 5) {
-    return textWordsArr[1];
-  }
-
-  if (b === 1) {
-    return textWordsArr[0];
-  }
-  return textWordsArr[2];
-};
-
-const generateData = (count) => {
-  const pinsData = [];
-
-  for (let i = 0; i < count; i++) {
-    const locationX = getRandomInRange(pinsPosLimits.x.min, pinsPosLimits.x.max);
-    const locationY = getRandomInRange(pinsPosLimits.y.min, pinsPosLimits.y.max);
-    const time = getRandomElement(TIMES);
-    const type = getRandomElement(APARTMENT_TYPES);
-    const apartmentType = typesMap[type];
-
-    pinsData.push({
-      author: {
-        avatar: `img/avatars/user0${i + 1}.png`
-      },
-      offer: {
-        title: `${getRandomElement(titlesMap[apartmentType])}`,
-        address: `${locationX}, ${locationY}`,
-        price: getRandomPrice(), // Temporary random value
-        type: `${type}`,
-        rooms: getRandomInRange(MIN_ROOMS_COUNT, MAX_ROOMS_COUNT), // Temporary value
-        guests: getRandomInRange(MIN_GUESTS_COUNT, MAX_GUESTS_COUNT), // Temporary value
-        checkin: `${time}`, // Temporary value
-        checkout: `${time}`, // Temporary value
-        features: getRandomArr(FEATURES),
-        description: `Здесь должно быть описание`,
-        photos: getRandomArr(PHOTOS_SRC),
-        location: {
-          x: locationX,
-          y: locationY
-        }
-      }
-    });
-  }
-
-  return pinsData;
-};
-
-const setIdToPins = (pinsArr) => {
-  const newArr = pinsArr.map((elem, index) => Object.assign({id: index}, elem));
-
-  return newArr;
-};
-
-const pinsData = generateData(OFFERS_COUNT);
-const pins = setIdToPins(pinsData);
 
 const createPin = (pin) => {
   const mapPin = mapPinTemplate.cloneNode(true);
@@ -300,8 +131,8 @@ const createCard = (pin) => {
   const popupCloseButton = popupCard.querySelector(`.popup__close`);
   const featureList = popupCard.querySelector(`.popup__features`);
   const photosContainer = popupCard.querySelector(`.popup__photos`);
-  const roomsDecl = declTextByNumber(rooms, ROOMS_DECLENSION);
-  const guestsDecl = declTextByNumber(guests, GUESTS_DECLENSION);
+  const roomsDecl = window.util.declTextByNumber(rooms, ROOMS_DECLENSION);
+  const guestsDecl = window.util.declTextByNumber(guests, GUESTS_DECLENSION);
   const capacity = `${rooms} ${roomsDecl} для ${guests} ${guestsDecl}`;
 
   popupCard.querySelector(`.popup__avatar`).src = avatar;
@@ -332,7 +163,7 @@ const createCard = (pin) => {
 const placePins = () => {
   const pinFragment = document.createDocumentFragment();
 
-  pins.forEach((elem) => {
+  window.data.pins.forEach((elem) => {
     pinFragment.appendChild(createPin(elem));
   });
 
@@ -375,8 +206,8 @@ const activatePage = (evt) => {
     map.classList.remove(`map--faded`);
     adForm.classList.remove(`ad-form--disabled`);
 
-    toggleFormElements(mapFiltersForm);
-    toggleFormElements(adForm);
+    window.util.toggleFormElements(mapFiltersForm);
+    window.util.toggleFormElements(adForm);
     setAddress();
     placePins();
 
@@ -397,8 +228,8 @@ const deactivatePage = () => {
     adForm.classList.add(`ad-form--disabled`);
   }
 
-  toggleFormElements(mapFiltersForm, true);
-  toggleFormElements(adForm, true);
+  window.util.toggleFormElements(mapFiltersForm, true);
+  window.util.toggleFormElements(adForm, true);
   setAddress();
 
   mainPin.addEventListener(`mousedown`, activatePage);
@@ -411,7 +242,7 @@ const onPinClick = (evt) => {
 
   if (pinButton && !evt.target.classList.contains(`map__pin--main`) && !pinButton.classList.contains(`map__pin--active`)) {
     const buttonId = parseInt(pinButton.dataset.id, 10);
-    const currentCardData = pins.find((item) => (item.id === buttonId));
+    const currentCardData = window.data.pins.find((item) => (item.id === buttonId));
 
     placeCard(currentCardData);
 
---
+++ js/util.js
@@ -0,0 +1,70 @@
+'use strict';
+
+(() => {
+  const toggleFormElements = (parentElem, state = false) => {
+    for (let elem of parentElem.children) {
+      elem.disabled = state;
+    }
+  };
+
+  const shuffleArray = (arr) => {
+    const newArr = arr.slice();
+
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+    }
+
+    return newArr;
+  };
+
+  const getRandomElement = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
+  const getRandomInRange = (min, max) => {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+  };
+
+  const getRandomArr = (arr) => {
+    const selectedElements = [];
+
+    if (arr.length !== 0) {
+      const randomElementsCount = getRandomInRange(1, arr.length);
+      const allElements = shuffleArray(arr);
+
+      for (let i = 0; i < randomElementsCount; i++) {
+        selectedElements.push(allElements[i]);
+      }
+    }
+
+    return selectedElements;
+  };
+
+  const declTextByNumber = (number, textWordsArr) => {
+    const a = Math.abs(number) % 100;
+    const b = number % 10;
+
+    if (a > 10 && a < 20) {
+      return textWordsArr[2];
+    }
+
+    if (b > 1 && b < 5) {
+      return textWordsArr[1];
+    }
+
+    if (b === 1) {
+      return textWordsArr[0];
+    }
+    return textWordsArr[2];
+  };
+
+  window.util = {
+    toggleFormElements,
+    shuffleArray,
+    getRandomElement,
+    getRandomInRange,
+    getRandomArr,
+    declTextByNumber,
+  };
+})();
