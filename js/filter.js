128744cfa967d0683b60255e7ef4f3cf20bd8081
AlexM <a.morozov.work@mail.ru>
2020-11-05 16:02:54 +0300

    filter.js fix

--- js/filter.js
+++ js/filter.js
@@ -57,13 +57,13 @@ const getFilteredData = (data) => {
   const filteredData = [];
 
   for (let i = 0; i < data.length; i++) {
-    if (filteredData.length >= MAX_PINS_COUNT) {
-      return filteredData;
-    }
-
     if (filterByType(data[i]) && filterByPrice(data[i]) && filterByRoomsCount(data[i])
       && filterByGuestsCount(data[i]) && filterByFeatures(data[i])) {
       filteredData.push(data[i]);
+    }
+
+    if (filteredData.length === MAX_PINS_COUNT) {
+      return filteredData;
     }
   }
 
